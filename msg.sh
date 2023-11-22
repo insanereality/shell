f5d16413bf1a72b3c0f6829c19ff59c3f4573262
insanereality <59973976+insanereality@users.noreply.github.com>
2023-11-22 09:37:53 -0300

    Create msg.sh

---
+++ msg.sh
@@ -0,0 +1,20 @@
+###CORES###
+C_DISABLED="\033[0m"
+C_RED="\033[1;31m"
+C_GREEN="\033[0;32m"
+C_YELLOW="\033[0;33m"
+C_CYAN="\033[0;36m"
+RESET="\e[0m"
+
+echo " "
+echo -e ${C_RED}"*****Aliases para administração do Nginx/xCALLY*****"${RESET}
+echo " "
+echo -e ${C_RED}"Digite xcallylogs para o diretório de logs do xCALLY"${RESET}
+echo -e ${C_RED}"Digite nginxlogs para o diretório de logs do NGINX"${RESET}
+echo -e ${C_RED}"Digite nginxerrorlog para visualizar o log de erro do NGINX"${RESET}
+echo -e ${C_RED}"Digite nginxaccesslog para visualizar o log de acesso do NGINX"${RESET}
+echo -e ${C_RED}"Digite agentesxcally para visualizar o número de agentes conectados"${RESET}
+echo -e ${C_RED}"Digite requisicoesip para visualizar o n. de requisições por IP"${RESET}
+echo -e ${C_RED}"Digite 'clstatus' para verificar o status do Cluster"${RESET}
+echo -e ${C_RED}"Digite 'clresources' para mostrar os serviços do Pacemaker em execução"${RESET}
+echo " "
