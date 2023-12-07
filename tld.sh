d04244af7dcd923a395f34a0c638a5568163db38
insanereality <59973976+insanereality@users.noreply.github.com>
2023-12-07 11:00:53 -0300

    Update tld.sh

--- tld.sh
+++ tld.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 #!/bin/bash
 ###CORES###
 C_DISABLED="\033[0m"
@@ -16,19 +15,19 @@ RESET="\e[0m"
 
 clear
 cpuuse=$(cat /proc/loadavg | awk '{print $1}')
- 
+
 if [ "$cpuuse" > 80 ]; then
 
-	#SUBJECT="ATTENTION: CPU Load Is High on $(hostname) at $(date)"
+        #SUBJECT="ATTENTION: CPU Load Is High on $(hostname) at $(date)"
 
-	echo -e "${C_RED}CPU Current Usage is: $cpuuse%${RESET}"
-	echo
-	echo
-	echo -e "${C_GREEN}+------------------------------------------------------------------+${RESET}"
-	echo -e "                   ${C_GREEN}Top CPU Process Using top command${RESET}"
-	echo -e "${C_GREEN}+------------------------------------------------------------------+${RESET}"
+        echo -e "${C_RED}CPU Current Usage is: $cpuuse%${RESET}"
+        echo
+        echo
+        echo -e "${C_GREEN}+------------------------------------------------------------------+${RESET}"
+        echo -e "                   ${C_GREEN}Top CPU Process Using top command${RESET}"
+        echo -e "${C_GREEN}+------------------------------------------------------------------+${RESET}"
 
-	echo "$(top -bn1 | head -20)"
+        echo "$(top -bn1 | head -20)"
 fi
 
 echo
@@ -53,195 +52,216 @@ echo
 echo -e "${C_GREEN}//Asterisk status//${RESET}"
 echo -e "${C_GREEN}+------------------------------------------------------------------+${RESET}"
 echo
-systemctl status asterisk | grep -i "Active:"
+service asterisk status | grep -i "Active:"
 
 echo
 echo -e "${C_GREEN}//Motion  status//${RESET}"
 echo -e "${C_GREEN}+------------------------------------------------------------------+${RESET}"
 echo
-service status motion | grep -i "Active:"
+service motion status | grep -i "Active:"
 echo
 echo
- 
+
+echo
+echo -e "${C_GREEN}//PHP-FPM  status//${RESET}"
+echo -e "${C_GREEN}+------------------------------------------------------------------+${RESET}"
+echo
+service php7.4-fpm status | grep -i "Active:"
+echo
+echo
+
 #3)Verifica espaço
 #-----------------------
- 
+
 echo -e "${C_GREEN}++------------------------Total/Usado/Livre--------------------------------------+${RESET}"
 echo
-echo "       " 			hduse=$(df -h)
-				echo $hduse
+echo "       "
+                df -h
+echo
 
 echo -e "${C_GREEN}++---------------------------------------------------------------------------------+${RESET}"
 
 echo
 echo
-echo -e "${C_GREEN}++-------Os 20 maiores arquivos em /var/log---------------------------------+${RESET}"
-echo	
-				du -ah /var/log/ | sort -n -r | head -n 20
+echo -e "${C_GREEN}++-------Os 10 maiores arquivos em /var/log e /var/log/xcally---------------------------------+${RESET}"
+echo
+                                du -ah /var/log/ | sort -n -r | head -n 10
+                                echo
+                                du -ah /var/log/xcally | sort -n -r | head -n 10
 echo
 echo -e "${C_GREEN}++--------------------------------------------------------------------+${RESET}"
 echo
 
 #4)Verifica fila de e-mails
 
-	mailqueue=$(exim -bpc)
+#        mailqueue=$(exim -bpc)
 
-if	[  "$mailqueue" -gt 1000 ];
-	then
-		ec -d 7 root
-	else
-	echo "Número de e-mails na fila: $mailqueue"	
-fi
+#if      [  "$mailqueue" -gt 1000 ];
+#        then
+#                ec -d 7 root
+#        else
+#        echo "Número de e-mails na fila: $mailqueue"
+#fi
 
-echo
-echo
-												
+#echo
+#echo
+
 #5)WHM
 
-echo -e "${C_GREEN}//Login WHM//${RESET}"
-	
-whmlogin	
-												
+#echo -e "${C_GREEN}//Login WHM//${RESET}"
+
+#whmlogin
+
+#echo
 echo
-echo
-												
+read -p "Pressione qualquer tecla para continuar... " -n1 -s
+clear
+
 #)Propagação de DNS, NS, SOA, RegistroBR
 
 #echo -e "${C_GREEN}++-------Propagação de DNS, NS, SOA, RegistroBR, Blacklists------------------------------+${RESET}"
 #echo
-#	echo -e ${C_YELLOW}Qual o domínio?${RESET}:
-#	echo
-#	read dom
-#echo	
-#	echo -e "	Propagação de DNS (A): https://www.whatsmydns.net/#A/${dom}"
-#	echo -e "	Nameservers: https://www.whatsmydns.net/#NS/${dom}"
-#	echo -e "	Registro SOA: https://www.whatsmydns.net/#SOA/${dom}"
-#	echo -e "	RegistroBR: https://hetrixtools.com/blacklist-check/${dom}"
-#	echo -e "	Checksite: checksites -d ${dom}"
-#echo	
+#       echo -e ${C_YELLOW}Qual o domínio?${RESET}:
+#       echo
+#       read dom
+#echo
+#       echo -e "       Propagação de DNS (A): https://www.whatsmydns.net/#A/${dom}"
+#       echo -e "       Nameservers: https://www.whatsmydns.net/#NS/${dom}"
+#       echo -e "       Registro SOA: https://www.whatsmydns.net/#SOA/${dom}"
+#       echo -e "       RegistroBR: https://hetrixtools.com/blacklist-check/${dom}"
+#       echo -e "       Checksite: checksites -d ${dom}"
+#echo
 #echo -e "${C_GREEN}Verificar blacklists${RESET}"
 #
-#	ip1=$(hostname -I | cut -d" " -f1)
-#	ip2=$(hostname -I | cut -d" " -f2)
+#       ip1=$(hostname -I | cut -d" " -f1)
+#       ip2=$(hostname -I | cut -d" " -f2)
 #echo
-#	echo -e "	https://hetrixtools.com/blacklist-check/${ip1}"
-#	echo -e "	https://hetrixtools.com/blacklist-check/${ip2}"
+#       echo -e "       https://hetrixtools.com/blacklist-check/${ip1}"
+#       echo -e "       https://hetrixtools.com/blacklist-check/${ip2}"
 #echo
 #echo -e "${C_GREEN}++--------------------------------------------------------------------+${RESET}"
 
+while : ; do
 echo
-echo -e " ${C_YELLOW}O que deseja fazer?${RESET}"
+echo -e "${C_YELLOW}O que deseja fazer?${RESET}"
 echo
-	echo -e "1) Limpar fila de e-mails"
-	echo -e "2) Reload na config do NGINX"
-	echo -e "3) Reiniciar NGINX"
-	echo -e "4) Reiniciar Asterisk"
-	echo -e "5) Reiniciar Motion"
-	echo -e "6) Instalar SPF"
-	echo -e "7) Trocar IP de saída do EXIM"
-	echo -e "8) Rodar HIGHLOAD"
-	echo -e "9) Instalar scan e rodar"
-	echo -e "10) Remover FPM"
-	echo -e "11) Reset Root"
-	echo -e "12) Número de requisições (netstat)"
-	echo -e "13) NGINX error log"
-	echo -e "14) Forçar SSL"
-	echo -e "15) Pesquisa GeoIP"
+        echo -e "  1) Visualizar número de agentes no xCally"
+        echo -e "  2) Reload na config do NGINX"
+        echo -e "  3) Reiniciar NGINX"
+        echo -e "  4) Reiniciar Asterisk"
+        echo -e "  5) Reiniciar Motion"
+        echo -e "  6) Reiniciar PHP-FPM"
+        #echo -e "7) Trocar IP de saída do EXIM"
+        #echo -e "8) Rodar HIGHLOAD"
+        #echo -e "9) Instalar scan e rodar"
+        #echo -e "10) Remover FPM"
+        #echo -e "11) Reset Root"
+        echo -e "  7) Número de requisições (netstat)"
+        #echo -e "13) NGINX error log"
+        #echo -e "14) Forçar SSL"
+        #echo -e "15) Pesquisa GeoIP"
+        echo -e
+        echo -e "${C_YELLOW}Digite a opção desejada: ${RESET}"
 
 read continue0
 case $continue0 in
 
 1)
-	echo -e "${C_GREEN}Limpando fila de e-mails...${RESET}"
-	exim -bp | awk '/^ *[0-9]+[mhd]/{print "exim -Mrm " $3}' | bash
-	;;
+        echo -e "${C_GREEN}Visualizar número de agentes no xCally${RESET}"
+        clear
+        curl -X GET http://localhost:3335
+        ;;
 
-2)      
-	echo -e "${C_YELLOW}Recarregando NGINX...${RESET}"
-	systemctl reload nginx.service
-	;;
+2)
+        echo -e "${C_YELLOW}Recarregando NGINX...${RESET}"
+        systemctl reload nginx.service
+        ;;
 
 3)
-	echo -e "${C_GREEN}Reiniciando NGINX...${RESET}"
-	systemctl restart nginx.service
-	;;
+        echo -e "${C_GREEN}Reiniciando NGINX...${RESET}"
+        systemctl restart nginx.service
+        ;;
 
 4)
-	echo -e "${C_GREEN}Reiniciando Asterisk...${RESET}"
-	systemctl restart asterisk.service
-	;;	
+        echo -e "${C_GREEN}Reiniciando Asterisk...${RESET}"
+        systemctl restart asterisk.service
+        ;;
 
 5)
-	echo -e "${C_GREEN}Reiniciando Motion...${RESET}"
-	systemctl restart motion.service
-	;;
+        echo -e "${C_GREEN}Reiniciando Motion...${RESET}"
+        systemctl restart motion.service
+        ;;
 
 6)
-	echo -e "${C_GREEN}Instalando SPF${RESET}"
-	for users in $(cat /etc/trueuserdomains | cut -d: -f2); do /usr/local/cpanel/bin/spf_installer $users; done
-	;;
+        echo -e "${C_GREEN}Reiniciando PHP-FPM${RESET}"
+        systemctl restart php7.4-fpm.service
+        ;;
 
-7)
-	echo -e "${C_GREEN}Trocar IP de saída do EXIM${RESET}"
-	bash <(curl -sk https://git.hostgator.com.br/chel/hgscripts/raw/master/patolino.sh)
-	;;
+#7)
+#        echo -e "${C_GREEN}Trocar IP de saída do EXIM${RESET}"
+#        bash <(curl -sk https://git.hostgator.com.br/chel/hgscripts/raw/master/patolino.sh)
+#        ;;
 
 8)
-	echo -e "${C_GREEN}Rodar HIGHLOAD${RESET}"
-	highload() { DAY=$(date -d "$1" +%m/%d) ; sys-snap ${DAY} | awk '!/proc_rstate/{ if ( $2 > 100 ) { print $1"\t\033[1;91m"$2"\033[0m" } else if ( $2 > 75 ) { print $1"\t\033[0;31m"$2"\033[0m" } else if ( $2 > 50 ) { print $1"\t\033[0;93m"$2"\033[0m" } else if ( $2 > 10 ) { print $1"\t\033[0;33m"$2"\033[0m" } else if ( $2 > 1 ) { print $1"\t"$2 } }' ; } ; highload today
-		;;  
+        echo -e "${C_GREEN}Rodar HIGHLOAD${RESET}"
+        highload() { DAY=$(date -d "$1" +%m/%d) ; perl /root/sys-snap.pl ${DAY} | awk '!/proc_rstate/{ if ( $2 > 100 ) { print $1"\t\033[1;91m"$2"\033[0m" } else if ( $2 > 75 ) { print $1"\t\033[0;31m"$2"\033[0m" } else if ( $2 > 50 ) { print $1"\t\033[0;93m"$2"\033[0m" } else if ( $2 > 10 ) { print $1"\t\033[0;33m"$2"\033[0m" } else if ( $2 > 1 ) { print $1"\t"$2 } }' ; } ; highload today
+                ;;
 
 
 9)
-	echo -e "${C_GREEN}Instalar scan e rodar${RESET}"
-	wget -q -O /root/bin/scan http://hgfix.net/paste/view/raw/df3c7e8e; chmod 100 /root/bin/scan
-	screen -d -m
+        echo -e "${C_GREEN}Instalar scan e rodar${RESET}"
+        wget -q -O /root/bin/scan http://hgfix.net/paste/view/raw/df3c7e8e; chmod 100 /root/bin/scan
+        screen -d -m
 ;;
 
-10)	
-	echo -e "${C_GREEN}Remover FPM${RESET}"
-	find /var/cpanel/userdata/ -iname "*fpm*" -exec mv {}{,-bkp} \;
-	/scripts/rebuildhttpdconf
-	systemctl reload httpd
+10)
+        echo -e "${C_GREEN}Remover FPM${RESET}"
+        find /var/cpanel/userdata/ -iname "*fpm*" -exec mv {}{,-bkp} \;
+        /scripts/rebuildhttpdconf
+        systemctl reload httpd
 ;;
 
 11)
 echo
-	echo -e "${C_GREEN}Reset ROOT${RESET}"
-	passwd=$(openssl rand -base64 8)
-	echo "Usuário: root"
-	echo "Senha: ${passwd}"
+        echo -e "${C_GREEN}Reset ROOT${RESET}"
+        passwd=$(openssl rand -base64 8)
+        echo "Usuário: root"
+        echo "Senha: ${passwd}"
 ;;
 
-12)
+7)
 echo
-	echo -e "${C_GREEN}Número de requisições por IP (netstat)${RESET}"
-	netstat -n|egrep ":80|:443|:8080|:22"|cut -c 45-|cut -f 1 -d ':'|sort|uniq -c|sort -nr|more
+        echo -e "${C_GREEN}Número de requisições por IP (netstat)${RESET}"
+        clear
+        netstat -n |cut -c 45- |cut -f 1 -d ':'| sort|uniq -c |sort -nr | head
 ;;
 
 13)
 echo
-	echo -e "${C_GREEN}NGINX error log${RESET}"
-	/var/log/nginx/error.log
+        echo -e "${C_GREEN}NGINX error log${RESET}"
+        /var/log/nginx/error_log
 ;;
 
 14)
 echo
-	echo -e "${C_GREEN}Forçar SSL${RESET}"
-	/usr/local/cpanel/bin/autossl_check_cpstore_queue --force
+        echo -e "${C_GREEN}Forçar SSL${RESET}"
+        /usr/local/cpanel/bin/autossl_check_cpstore_queue --force
 ;;
-			
+
 15)
 echo
-	echo -e "${C_GREEN}Pesquisar GeoIP${RESET}"
+        echo -e "${C_GREEN}Pesquisar GeoIP${RESET}"
         echo Qual o IP?:
-	read GeoIP
-	echo -e "	https://www.geodatatool.com/pt/?ip=${GeoIP}"
+        read GeoIP
+        echo -e "       https://www.geodatatool.com/pt/?ip=${GeoIP}"
 ;;
-			
+
 16)
-	echo -e "${C_GREEN}Forçar SSL${RESET}"
-	/usr/local/cpanel/bin/autossl_check_cpstore_queue --force
+        echo -e "${C_GREEN}Forçar SSL${RESET}"
+        /usr/local/cpanel/bin/autossl_check_cpstore_queue --force
 ;;
-			
-esac
+
+        esac
+        echo -e
+        echo -e
+done
